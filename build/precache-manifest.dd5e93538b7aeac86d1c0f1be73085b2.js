747a6f3536dce763b19ab09fe4b054af36e4b292
Andrey <ozornin404@yandex.ru>
2019-10-11 16:20:57 +0300

    add /build directory

---
+++ build/precache-manifest.dd5e93538b7aeac86d1c0f1be73085b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163226770d2a63fda1b638a734684701",
+    "url": "/sequencer/index.html"
+  },
+  {
+    "revision": "e56b2ed7a1b5156c383a",
+    "url": "/sequencer/static/css/main.6061e0a6.chunk.css"
+  },
+  {
+    "revision": "6ceb8e91af9a7bbd3743",
+    "url": "/sequencer/static/js/2.a5d5e210.chunk.js"
+  },
+  {
+    "revision": "e56b2ed7a1b5156c383a",
+    "url": "/sequencer/static/js/main.59aac072.chunk.js"
+  },
+  {
+    "revision": "f1a28863182e5cbf59fa",
+    "url": "/sequencer/static/js/runtime-main.a4c1fe0b.js"
+  }
+]);
